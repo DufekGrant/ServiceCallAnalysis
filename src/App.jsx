53d7b0ff4e3bd41045078cec98af03c8bbcb3c4d
DufekGrant <117484162+DufekGrant@users.noreply.github.com>
2025-11-10 01:03:41 -0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
 import ServiceCallAnalysis from './components/ServiceCallAnalysis'
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 10000 
 
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
 function App() {
   return <ServiceCallAnalysis />
 }
